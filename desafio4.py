5d62057f8853490d0c1894b9d04a41a60393f1da
alejo15 <emmanuel.alej15@gmail.com>
2024-06-16 00:38:35 -0400

    Reto 14 de junio

---
+++ desafio4.py
@@ -0,0 +1,34 @@
+"""Ordenar lista: Escribe un programa que ordene una lista de números dada por el usuario en orden acendente"""
+def ordenar():
+    
+    lista = []
+    
+    while True:
+        print("Si ya no desea ingresar datos presiona (enter)")
+        entrada = input("ingrese un número: ")  
+        
+        if entrada == "":
+            break  
+        
+        try:
+            numero = float(entrada)
+            lista.append(numero)
+            
+        
+        except ValueError:
+            print("El dato no es valido, Por favor ingrese un número.")
+    
+    lista_ordenada = sorted(lista)  
+    return lista_ordenada     
+        
+
+lista_ordenada = ordenar()
+
+print("La lista ordenada de forma acendente: ", lista_ordenada)
+
+
+# lista = [int(input("Ingrese los números: "))]
+
+# lista_ordenada = sorted(lista)
+
+# print(lista), print(lista_ordenada)
